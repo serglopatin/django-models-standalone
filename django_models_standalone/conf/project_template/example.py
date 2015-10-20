754c889642dae7b253ec056f15a9f8d392ab9432
lopatin.sergei <lopatin.sergei@gmail.com>
2015-10-20 11:03:52 +0800

    removed reset_db_connection

--- django_models_standalone/conf/project_template/example.py
+++ django_models_standalone/conf/project_template/example.py
@@ -1,6 +1,5 @@
 import os
 import django
-from models_standalone import reset_db_connection, settings
 
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "models_standalone.settings")
 django.setup()
@@ -9,16 +8,4 @@ from models_standalone.models import SampleModel
 
 if __name__=="__main__":
 
-
-	"""
-	This is sample application that uses django models.
-	When DEBUG is True, we must manually reset db connection  after each query.
-	"""
-
-	for i in range(3):
-
-		print "SampleModel objects: " + str(len(SampleModel.objects.all()))
-
-		if settings.DEBUG:
-			reset_db_connection()
-
+	print "SampleModel objects: " + str(len(SampleModel.objects.all()))
--- django_models_standalone/conf/project_template/models_standalone/__init__.py
+++
@@ -1,5 +0,0 @@
-from django import db as django_db
-
-# when DEBUG is True we need to reset connection in standalone application, otherwise you will have memory leaks
-def reset_db_connection():
-	django_db.connection.close()
